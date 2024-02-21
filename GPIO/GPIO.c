f9be95710e3f6f9cf1d8b9b2b965b7eba22a54ec
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-02-21 08:19:25 +0530

    Cleanup

--- GPIO/GPIO.c
+++ GPIO/GPIO.c
@@ -5,12 +5,15 @@
 #define LED_PIN_ON      (1 << PB4)
 
 int main(void) {
-    DDRB = 0x3E;
+    DDRB |= LED_PIN_ON;                     // Set LED pin as output
+    DDRB &= ~SWITCH_PIN;                    // Set Switch pin as input
+
+    PORTB |= SWITCH_PIN;                    // Enable pull up resistor at LED pin
 
     while(1) {
-        if( PINB & SWITCH_PIN ) {
+        if( PINB & SWITCH_PIN ) {           // If the switch pin is HIGH
             PORTB |= LED_PIN_ON;
-            _delay_ms(10);
+            _delay_ms(10);                  // switch debouncing delay
         } else {
             PORTB &= ~LED_PIN_ON;
             _delay_ms(10);
