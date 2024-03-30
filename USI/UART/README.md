d88bcb87d1dcaa898ea017091b7a112e5f43308b
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-03-30 07:46:10 +0530

    Added readme and serial read script cleanup

---
+++ USI/UART/README.md
@@ -0,0 +1,11 @@
+How to use serial_read.py
+
+Show the data read as plain string
+`
+python serial_read.py
+`
+
+Show the data read as hex
+`
+python serial_read.py -h
+`
--- USI/UART/serial_read.py
+++ USI/UART/serial_read.py
@@ -3,29 +3,30 @@ import time
 import sys
 
 # Define the serial port name and baudrate
-serial_port = '/dev/tty.usbserial-1410'  # Change this to your serial port name
-baud_rate = 2400  # Change this to match your device's baud rate
+serial_port = '/dev/tty.usbserial-1410'  # TODO: Change this to your serial port name
+baud_rate = 2400                         # Change this to match your device's baud rate
 
 # Initialize the serial port
 ser = serial.Serial(serial_port, baud_rate)
+
+# Process command line arguments
 print_hex = False
+if len(sys.argv) > 1 and sys.argv[1] == '-h':
+    print_hex = True
+
+# Read serial port data and print
 try:
     while True:
-        # ser.write(b'Hello, Arduino!\n')  # Change the message as needed
-
-        # Wait for a short delay (optional)
-        # ser.flush()  # Ensure all data is sent before continuing
-
-        # Print a confirmation message
-        # print("Data sent successfully")
         if print_hex:
             char = ser.read(1).hex()
             print('0x' + char, end=" ")
         else:
             char = ser.read(1)
-            print(char.decode('utf-8'), end="")
+            try:
+                print(char.decode('utf-8'), end="")
+            except UnicodeDecodeError:
+                pass
         sys.stdout.flush()
-        # print(strdata.ljust(30), " hex: ", data)
 
 except KeyboardInterrupt:
     ser.close()
