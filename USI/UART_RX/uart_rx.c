89f57adefb7a50f4991879f54347c5986f3ac7cc
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-04-09 16:33:21 +0530

    UART receive testing started

--- USI/UART_RX/uart_rx.c
+++ USI/UART_RX/uart_rx.c
@@ -44,35 +44,6 @@
 
 #define     FULL_BIT_WIDTH      (F_CPU / (BAUD_RATE * DIVISOR))
 #define     HALF_BIT_WIDTH      (FULL_BIT_WIDTH / 2)
-#if 0
-// Enum to keep the state of output line
-typedef enum {
-    AVAILABLE,
-    PROC_BUFF1,
-    PROC_BUFF2
-} line_status_t;
-
-volatile static line_status_t line_status = AVAILABLE;
-
-static uint8_t  data_buff;
-/**
- * @brief Initialize UART, set output pins
- * 
- * @param   None
- * @return  None
- */
-#endif
-
-void test_fun() {
-    PORTB |= (1 << PB2);
-    _delay_ms(100);
-    PORTB &= ~(1 << PB2);
-    _delay_ms(100);
-    PORTB |= (1 << PB2);
-    _delay_ms(100);
-    PORTB &= ~(1 << PB2);
-    _delay_ms(100);
-}
 
 static void setup_usi_uart() {
     DDRB &= ~(1 << DATA_IN_PIN);               // Set DI pin as input
@@ -85,17 +56,19 @@ static void setup_usi_uart() {
 
 void on_uart_data_rx() {
     TCCR0A |= (1 << WGM01);                 // Clear timer on compare match
-    TCCR0B |= (1 << CS01);                  // Set prescaler 8 to clk
     GTCCR  |= (1 << PSR0);                  // Reset prescaler
-    OCR0A = HALF_BIT_WIDTH;            // TODO: find the correct value to go to the middle of bit 0
+    OCR0A = HALF_BIT_WIDTH;                 // TODO: find the correct value to go to the middle of bit 0
     TCNT0 = 0;                              // Start counting from 0
     TIFR |= (1 << OCF0A);                   // Clear Timer0 compare match interrupt flag
     TIMSK |= (1 << OCIE0A);                 // Enable Timer0 compare match interrupt
+    TCCR0B |= (1 << CS01);                  // Set prescaler 8 to clk
 }
 
 ISR(PCINT0_vect) {
-    GIMSK &= ~(1 << PCIE);                  // Disable the pin changeinterrupt
-    on_uart_data_rx();
+    if(0 == (PINB & (1 << PB0))) {
+        GIMSK &= ~(1 << PCIE);                  // Disable the pin changeinterrupt
+        on_uart_data_rx();
+    }
 }
 
 ISR(TIMER0_COMPA_vect) {
@@ -136,74 +109,14 @@ ISR (USI_OVF_vect) {
     GIMSK |= (1 << PCIE);                   // Enable pin change interrupt
 }
 
-#if 0
-/**
- * @brief Initialize sending a single byte
- *
- * The USI over flow ISR will be invoked once the first part of total data sent.
- *
- * @param  data Data to be sent
- * @return
- */
-static uint8_t uart_recv_byte() {
-    while(PORTB & 0x01 == 1);                   // Wait until the line become low(start bit)
-
-    /* Configure timer/counter0 to use as the clock of USI */
-    TCCR0A |= (1 << WGM01);                     // Clear timer on compare match
-    TCCR0B |= (1 << CS01);                      // Set prescaler to clk
-    GTCCR  |= (1 << PSR0);                      // Reset prescaler
-    OCR0A = F_CPU/(BAUD_RATE * DIVISOR);        // 1000000/(2400 * 8) = 52
-    TCNT0 = 0;                                  // Count Timer/Counter0 from 0
-
-    /* Configure USI 3 wire mode and use PB2 as data output pin */
-    USICR |= (1 << USIOIE) |                    // Enabled overflow interrupt
-             (0 << USIWM1) | (1 << USIWM0) |    // Enable 3 wire mode - data output to PB2
-             (0 << USICS1) | (1 << USICS0) | (0 << USICLK);  // Select Timer0 Compare match
-                                                             // as USI Clock source.
-
-    USISR |= (1 << USIOIF) |                    // Clear overflow interrupt flag
-             (16 - 8);                          // we have 8 bits to send; the overflow interrupt
-                                                // trigger when 8 bits are sent. 
-}
-
-/**
- * ISR for USI counter overflow. This will be called upon completing all data in the USIDR sent.
- */
-ISR (USI_OVF_vect) {
-    if( PROC_BUFF1 == line_status ) {
-        USIDR = 0x7F | (data_buff << 7);        // remaining bit + stop bits
-        USISR = (1 << USIOIF) |                 // Clear USI overflow interrupt flag
-                (16 - 2);                       // Set USI counter to send last data bit + 1 stop bits
-        line_status = PROC_BUFF2;
-    } else {
-        PORTB |= (1 << DATA_OUT_PIN);           // Ensure output is high
-        USICR = 0;                              // Disable USI.
-        USISR |= (1 << USIOIF);                 // clear interrupt flag
-        line_status = AVAILABLE;
-    }
-}
-
-#endif
 /**
  * The main function
  */
 int main(void) {
-    // uint8_t data;
     DDRB |= (1 << PB2);
-    PORTB &= ~(1 << PB2);
-    _delay_ms(1000);
-    PORTB |= (1 << PB2);
     setup_usi_uart();
 
     while(1) {
-#if 0
-        data = uart_recv_byte();
-        if(data == 0x10) {
-            PORTB != (1 << PB2);
-        } else {
-            PORTB &= ~(1 << PB2);
-        }
-#endif
     }
     return 0;
 }
