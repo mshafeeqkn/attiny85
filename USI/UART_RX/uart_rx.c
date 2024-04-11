c0320c35b8cea5fba5bc52f29774d8eacbf3e54c
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-04-11 21:22:56 +0530

    Added function header

--- USI/UART_RX/uart_rx.c
+++ USI/UART_RX/uart_rx.c
@@ -44,6 +44,10 @@
 
 #define     FULL_BIT_WIDTH      (F_CPU / (BAUD_RATE * DIVISOR))
 #define     HALF_BIT_WIDTH      (FULL_BIT_WIDTH / 2)
+// 53 is the number of clock ticks elapsed to
+// start the timer1 after start bit is detected.
+// This value is calculated with the help of timer/counter1.
+#define     START_BIT_FIX       (53 / DIVISOR)
 
 typedef enum {
     AVAILABLE,
@@ -54,6 +58,12 @@ volatile static line_status_t line_status = AVAILABLE;
 
 static uint8_t  data_buff = 0;
 
+/**
+ * @brief Blink the LED number of times
+ * 
+ * @param c How many times the LED is to blink?
+ * @return None
+ */ 
 void blink_led(uint8_t c) {
     while(c) {
         PORTB |= (1 << PB4);
@@ -65,6 +75,12 @@ void blink_led(uint8_t c) {
     data_buff = 0;
 }
 
+/**
+ * @brief Initialize UART for the reception.
+ * 
+ * @param   None
+ * @return  None
+ */
 static void setup_usi_uart_rx() {
     DDRB &= ~(1 << DATA_IN_PIN);               // Set DI pin as input
     PORTB |= (1 << DATA_IN_PIN);               // Set the output high in idle
@@ -74,16 +90,26 @@ static void setup_usi_uart_rx() {
     sei();                                     // Enable global interrupt
 }
 
+/**
+ * @brief Setup the timer to reach the half bit width after the start bit is detected
+ * 
+ * @param None
+ * @param None
+ */
 void on_uart_data_rx() {
     TCCR0A |= (1 << WGM01);                 // Clear timer on compare match
     GTCCR  |= (1 << PSR0);                  // Reset prescaler
-    OCR0A = HALF_BIT_WIDTH - (53/DIVISOR);  // TODO: find the correct value to go to the middle of bit 0
+    OCR0A = HALF_BIT_WIDTH - START_BIT_FIX; // Set the counter value
     TCNT0 = 0;                              // Start counting from 0
     TIFR |= (1 << OCF0A);                   // Clear Timer0 compare match interrupt flag
     TIMSK |= (1 << OCIE0A);                 // Enable Timer0 compare match interrupt
     TCCR0B |= (1 << CS01);                  // Set prescaler 8 to clk
 }
 
+/**
+ * ISR for start bit of the data (External interrupt).
+ * This will be called upon receiving the data.
+ */
 ISR(PCINT0_vect) {
     // start_timer1();
     if(0 == (PINB & (1 << PB0))) {
@@ -92,6 +118,10 @@ ISR(PCINT0_vect) {
     }
 }
 
+/**
+ * ISR Timer0, This will be called after half bit width
+ * is completed after the start bit is detected.
+ */
 ISR(TIMER0_COMPA_vect) {
     TIMSK &= ~(1 << OCIE0A);                // Enable Timer0 compare match interrupt
 
@@ -117,6 +147,10 @@ static uint8_t reverse_byte (uint8_t byte) {
     return byte;
 }
 
+/**
+ * ISR for USI counter overflow.
+ * This will be called upon completing 1 byte rx.
+ */
 ISR (USI_OVF_vect) {
     data_buff = USIBR;
     USICR = 0;
